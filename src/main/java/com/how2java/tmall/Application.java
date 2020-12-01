1e9853a559a480cac30cf01110f31e84258267be
CharlesSu001 <894645804@qq.com>
2020-12-01 16:57:42 +0800

    tmall_springboot框架实现1.0

--- src/main/java/com/how2java/tmall/Application.java
+++ src/main/java/com/how2java/tmall/Application.java
@@ -30,11 +30,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableElasticsearchRepositories(basePackages= "com.how2java.tmall.es")
 @EnableJpaRepositories(basePackages = {"com.how2java.tmall.dao","com.how2java.tmall.pojo"})
 public class Application {
-    static {
-        PortUtil.checkPort(6379, "Redis服务端", true);
-        PortUtil.checkPort(9300,"ElasticSearch服务端",true);
-        PortUtil.checkPort(5601,"Kibana工具",true);
-    }
+//    static {
+//        PortUtil.checkPort(6379, "Redis服务端", true);
+//        PortUtil.checkPort(9300,"ElasticSearch服务端",true);
+//        PortUtil.checkPort(5601,"Kibana工具",true);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
